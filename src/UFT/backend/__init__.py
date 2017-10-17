c22b836bc8fdabbf77e1a10948da83f5796d9364
danzel.li <hardanimal@hotmail.com>
2017-10-17 16:21:54 +0800

    Add get latest revision from config database.

--- src/UFT/backend/__init__.py
+++ src/UFT/backend/__init__.py
@@ -6,4 +6,4 @@ __version__ = "0.1"
 __author__ = "@fanmuzhi, @boqiling"
 __all__ = ["load_config", "sync_config", "load_test_item"]
 
-from config_io import load_config, sync_config, load_test_item
+from config_io import load_config, sync_config, load_test_item, get_latest_revision
